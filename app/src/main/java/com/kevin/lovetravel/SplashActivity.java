c16fa4ac727a27d75d9e9d56d39bd185fbf9bae2
Benson <389089141@qq.com>
2016-10-19 12:00:59 +0800

    Initial commit

---
+++ app/src/main/java/com/kevin/lovetravel/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.kevin.lovetravel;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash);
+        Handler mHandler = new Handler();
+        mHandler.postDelayed(new splashHandler(),3000);
+    }
+
+
+    class splashHandler implements Runnable{
+
+        @Override
+        public void run() {
+            startActivity(new Intent(getApplication(),MainActivity.class));
+            SplashActivity.this.finish();
+        }
+    }
+}
